2cf3ed5525b1d2560e9db0a5387e3bea441be45d
WhichWho <1366505103@qq.com>
2022-10-14 23:55:08 +0800

    add make hard link bat

--- src/main/kotlin/CodeMain.kt
+++ src/main/kotlin/CodeMain.kt
@@ -1,8 +1,9 @@
 
 fun main(args: Array<String>) {
-    val src = "D:\\Program Files\\Genshin Impact\\Genshin Impact Game"
-    val dst = "C:\\Users\\hp\\Desktop\\GenshinImpact_3.0.51.1_beta"
-    val diff = "D:\\Download\\GenshinImpactBeta"
-    makeRetainLinks(src, dst, diff)
-    makeDiffPackage(src, dst, diff)
+    val src = "D:\\Program Files\\Genshin Impact\\Genshin Impact Game\\"
+    val dst = "C:\\Users\\hp\\Desktop\\GenshinImpact_3.0.51.1_beta\\"
+    val diff = "D:\\Download\\GenshinImpactBeta\\"
+//    makeRetainLinks(src, dst, diff)
+//    makeDiffPackage(src, dst, diff)
+    makeHardlinkBat(src, dst, "D:\\Download\\diff.bat")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,20 +7,64 @@ import kotlin.io.path.createLinkPointingTo
 const val PKG_VERSION = "pkg_version"
 
 fun main(args: Array<String>) {
-    if (args[0] == "-link" && args.size == 4) {
-        makeRetainLinks(args[1], args[2], args[3])
-    } else if (args.size == 3) {
+    if (args.size == 3) {
         makeDiffPackage(args[0], args[1], args[2])
+    } else if (args.size == 4) {
+        when (args[0]) {
+            "-link" -> makeRetainLinks(args[1], args[2], args[3])
+            "-bat" -> makeHardlinkBat(args[1], args[2], args[3])
+            else -> printHelp()
+        }
     } else {
-        println("""
+        printHelp()
+    }
+}
+
+private fun printHelp() {
+    println(
+        """
             use:
             generate diff package to output_folder
                 java -jar GenshinDiff.jar old_version_folder new_version_folder output_folder
             make hard link base on old_version_folder
                 java -jar GenshinDiff.jar -link old_version_folder new_version_folder output_folder
-        """.trimIndent())
+            make hard link bat
+                java -jar GenshinDiff.jar -bat old_version_folder new_version_folder output.bat
+        """.trimIndent()
+    )
+}
+
+fun makeHardlinkBat(src: String, dst: String, bat: String) {
+    val srcPkgFile = File(src, PKG_VERSION)
+    val dstPkgFile = File(dst, PKG_VERSION)
+    if (!srcPkgFile.exists() || !dstPkgFile.exists()) return
+    File(bat).outputStream().bufferedWriter().use { w ->
+        w.appendLine(
+            """
+        set src=$src
+        set dst=$dst
+        """.trimIndent()
+        )
+        w.newLine()
+        val dirs = mutableSetOf<String>()
+        val files = mutableListOf<String>()
+        val srcFiles = srcPkgFile.readInfo()
+        dstPkgFile.readInfo().forEach { info ->
+            val indexOf = srcFiles.indexOf(info)
+            if (indexOf == -1) return@forEach
+            val srcRemoteName = srcFiles[indexOf].remoteName
+            val folder = info.remoteName.substringBeforeLast("/", "")
+            if (folder != "") {
+                dirs += "mkdir \"%dst%${folder.flip()}\""
+            }
+            files += "mklink /H \"%dst%${info.remoteName.flip()}\" \"%src%${srcRemoteName.flip()}\""
+        }
+        dirs.forEach { w.appendLine(it) }
+        files.forEach { w.appendLine(it) }
     }
 }
+
+fun String.flip() = replace("/", "\\")
 
 fun makeRetainLinks(src: String, dst: String, diff: String) {
     makeRetainLinks(src, File(dst, PKG_VERSION), diff)
